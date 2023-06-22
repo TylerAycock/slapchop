126e6d1bd3d14945a7bd014c7d8f13bd6373d68b
Tyler Aycock <tyler.joseph.aycock@gmail.com>
2023-06-22 13:03:18 -0700

    updated

--- server/server.js
+++ server/server.js
@@ -7,8 +7,10 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('public'))
 
+app.use(express.static(`${__dirname}/public`))
 
+app.get('/',(req,resp)=>{
+    resp.status(200).sendFile(path.join(__dirname, '../public/landingpage.html'))
+})
 
-
-
-app.listen(4000, console.log(`Up and running on good ol 4000`))
+app.listen(4000, () => console.log(`server running on 4000`))
